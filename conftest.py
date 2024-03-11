9ce50a349509bb7c3a89b4fa324ec60474cec6c5
Beskha641 <moralov_av@mail.ru>
2024-03-11 21:04:24 +0300

    Убраны опции браузера игнорирующие ошибки ssl сертификата

--- conftest.py
+++ conftest.py
@@ -1,12 +1,8 @@
-
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+
 @pytest.fixture(scope='function')
 def browser():
-    options = Options()
-    options.add_argument('--ignore-certificate-errors')
-    options.add_argument('--ignore-ssl-errors')
-    browser = webdriver.Chrome(options=options)
+    browser = webdriver.Chrome()
     yield browser
     browser.quit()
